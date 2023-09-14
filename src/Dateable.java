7d57fa5e366b1ee83065a215b3dba944ad98329d
Eric <erict2026@student.cis.edu.hk>
2023-09-14 15:00:20 +0800

    Finished Assignment

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        int minimumAge = (age/2)+7;
+        System.out.println(age + " year olds should only date someone who is at least " + minimumAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Pages {
-
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int age = scanner.nextInt();
+        int minimumPages = 100-age;
+        System.out.println(age + " year olds should read at least " + minimumPages + " before giving up on a book.");
     }
 }
